bd74ff70e1b43c3d1d626679c031ac28eb395c09
nasrah01 <nasrah1@hotmail.co.uk>
2020-10-22 21:26:30 +0100

    navigation improvement

--- src/view/navigation.js
+++ src/view/navigation.js
@@ -7,7 +7,7 @@ export const links = () => {
     const lineTwo = document.querySelector('.navigation__two');
     const lineThree = document.querySelector('.navigation__three');
     const burger = document.querySelectorAll('.navigation__line');
-    const items = document.querySelectorAll('.navigation__content');
+    const items = document.querySelectorAll('.navigation__name');
     const screen = document.querySelector('.navigation__large');
     const hamburger = document.querySelector('.navigation__container');
 
@@ -24,8 +24,7 @@ export const links = () => {
     .to(lineThree, {rotation:45,  duration: .3}, '-=.3') 
     .to(burger, {css:{backgroundColor:"#594839", backgroundImage: 'linear-gradient(315deg, #594839 0%, #594839 74%)'}, duration: .1, ease:"Power1.easeOut"}, '-=.1')
     .fromTo(screen,{ css:{width: "0"}, ease: "Circ.easeIn"}, { css:{width: "100%"}, ease: "Circ.easeOut" , duration: .5}, '-=.1')
-    .staggerFrom(items, 0.5, {opacity: 0, y: 25, ease: "Power1.easeOut"}, 0.2, "-=0.7")
-    .to(items, 0.2, {css:{color:"#000"}});
+    .to(items, 0.5, {opacity: 1, ease: "Power1.easeOut"}, 0.2, "-=0.7")
 
 
     hamburger.addEventListener('click', () => {
@@ -33,39 +32,36 @@ export const links = () => {
             tl.play();
         } else {
             tl.reverse();
-        }
+        }    
     });
 
     items.forEach((item)  => {
         item.addEventListener('click',() => {
-            if (tl.reversed()) {
-                tl.play();
-            } else {
-                tl.reverse();
-            }
+            
+            tl.reverse();
+            
         });
-    })
+    }) 
 
-/* 
-            const mainNav = document.querySelectorAll('.navigation__ref');
+    const mainNav = document.querySelectorAll('.navigation__ref');
 
-        mainNav.forEach((list) => {
-            list.addEventListener('click', () => {
-                setTimeout(() => {
-                    removeHash();
-                }, 5);
-            });
+    mainNav.forEach((list) => {
+        list.addEventListener('click', () => {
+            setTimeout(() => {
+                removeHash();
+            }, 5);
         });
+    });
 
-        function removeHash() {
-            var uri = window.location.toString(); 
-        
-                    if (uri.indexOf("#") > 0) { 
-                        var clean_uri = uri.substring(0,  
-                                        uri.indexOf("#")); 
-        
-                        window.history.replaceState({},  
-                                document.title, clean_uri); 
-                    } 
-        }; */
+    function removeHash() {
+        var uri = window.location.toString(); 
+    
+                if (uri.indexOf("#") > 0) { 
+                    var clean_uri = uri.substring(0,  
+                                    uri.indexOf("#")); 
+    
+                    window.history.replaceState({},  
+                            document.title, clean_uri); 
+                } 
+    }; 
 };
